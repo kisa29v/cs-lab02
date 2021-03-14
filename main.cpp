5c3893c0a94b69a9107f4613519d3c042f258ad2
Alice (YablokovaVA) <v.yablokova29@gmail.com>
2021-03-14 17:27:08 +0300

    нахождение максимума

--- main.cpp
+++ main.cpp
@@ -8,7 +8,15 @@ int main()
     int a, b, sum;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
-     << "A - B = " << a - b << '\n'
-     << "A * B = " << a * b << '\n'
-     << "A / B = " << a / b << '\n';
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n';
+    if(a>b)
+    {
+        cout << a;
+    }
+    if(b>a)
+    {
+        cout << b;
+    }
 }
