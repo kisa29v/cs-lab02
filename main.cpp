87641855b72cd45b6ef37aee73ca2f6c57643101
Alice (YablokovaVA) <v.yablokova29@gmail.com>
2021-03-14 16:10:21 +0300

    деление2

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     int a, b, sum;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
-        << "A - B = " << a - b << '\n';
-        << "A * B = " << a * b << '\n';
-        << "A / B = " << a / b << '\n';
+     << "A - B = " << a - b << '\n'
+     << "A * B = " << a * b << '\n'
+     << "A / B = " << a / b << '\n';
 }
